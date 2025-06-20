a59189adc4d524f635711058265e0ddc94002e68
Paiva <danpaiva@live.com>
2025-06-20 13:56:21 -0300

    feat: add interface

---
+++ TodoMobileBff/TodoMobileBff/Services/Interfaces/ITodoClient.cs
@@ -0,0 +1,9 @@
+﻿using TodoMobileBff.Models;
+
+namespace TodoMobileBff.Services.Interfaces;
+
+public interface ITodoClient
+{
+    Task<IEnumerable<TodoItem>> GetAllTodoItemsAsync();
+    Task<TodoItem?> GetTodoItemByIdAsync(long id);
+}
