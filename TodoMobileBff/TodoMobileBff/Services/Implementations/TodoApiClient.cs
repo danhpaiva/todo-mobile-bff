59a3c18ed8011a99d39e8a2713c0662fb8e80f08
Paiva <danpaiva@live.com>
2025-06-20 14:02:20 -0300

    feat: add TodoClient

---
+++ TodoMobileBff/TodoMobileBff/Services/Implementations/TodoApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using TodoMobileBff.Models;
+using TodoMobileBff.Services.Interfaces;
+
+namespace TodoMobileBff.Services.Implementations;
+
+public class TodoApiClient : ITodoClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _baseApiUrl = "http://localhost:5097/api/TodoItems";
+
+    public TodoApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri(_baseApiUrl);
+    }
+
+    public async Task<IEnumerable<TodoItem>> GetAllTodoItemsAsync()
+    {
+        var response = await _httpClient.GetAsync(_baseApiUrl);
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<List<TodoItem>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    }
+
+    public async Task<TodoItem?> GetTodoItemByIdAsync(long id)
+    {
+        var response = await _httpClient.GetAsync($"{_baseApiUrl}/{id}");
+        if (response.StatusCode == System.Net.HttpStatusCode.NotFound) return null;
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<TodoItem>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    }
+}
